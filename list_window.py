4e9ae02948de45032434bd72e2f209a41db92752
James Nash <james.xxyzzy@gmail.com>
2023-07-14 09:47:39 +0200

    Update list_window.py

--- list_window.py
+++ list_window.py
@@ -3,25 +3,34 @@ from tkinter import ttk
 from tkinter.messagebox import showinfo
 
 # create the list window
-def list_window(the_list, title, completion):
-    window = tk.Toplevel()
-    window.title(title)
-    var = tk.Variable(value=the_list)
-    listbox = tk.Listbox(window, listvariable=var,
-        height=6, selectmode=tk.EXTENDED)
-    listbox.pack(expand=True, fill=tk.BOTH)
-
+class ListWindow:
+    this = None   # work_around
+    def __init__(self, the_list, title, completion, multiple=False):
+        ListWindow.this = self
+        self.completion = completion
+        self.multiple = multiple
+        self.window = tk.Toplevel()
+        self.window.title(title)
+        self.window.geometry("200x200")
+        self.var = tk.Variable(value=the_list)
+        self.listbox = tk.Listbox(self.window, listvariable=self.var,
+            height=6, selectmode=tk.EXTENDED)
+        self.listbox.pack(expand=True, fill=tk.BOTH)
+        self.listbox.bind('<<ListboxSelect>>', ListWindow.items_selected)
+    
+    @staticmethod
     def items_selected(event):
-        # get all selected indices
-        selected_indices = listbox.curselection()
-        # get selected items
-        selected_langs = ",".join([listbox.get(i) for i in selected_indices])
-        msg = f'You selected: {selected_langs}'
-        showinfo(title='Information', message=msg)
+        self = ListWindow.this
+        indices = self.listbox.curselection()
+        values = [self.listbox.get(i) for i in indices]
+        if not self.multiple:
+            self.window.destroy()
+        self.completion(values)
 
-    listbox.bind('<<ListboxSelect>>', items_selected)
 
 if __name__ == "__main__":
+    def list_window(the_list, title, completion):  
+        lw = ListWindow(the_list, title, completion)
     def get_result(values):
         print(values)
     def call_list_window():
@@ -29,6 +38,8 @@ if __name__ == "__main__":
         list_window(my_list, 'select', get_result)
 	    
     root = tk.Tk()
+    root.title('Test list window')
+    root.geometry("400x200")
     b = tk.Button(root, text='test list window', command=call_list_window)
     b.pack()
     root.mainloop()
