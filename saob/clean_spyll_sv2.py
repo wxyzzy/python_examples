eb3a0995f8efeb1f9bbbd47bbf45173cc6977335
James Nash <james.xxyzzy@gmail.com>
2023-08-05 16:34:33 +0200

    improve list making

--- saob/clean_spyll_sv2.py
+++ saob/clean_spyll_sv2.py
@@ -11,13 +11,36 @@ sfxs = None
 
 def conjugate_word(w, code):
     conj = []
+    if w[:5] == 'afron':
+        print(w)
     for c in code:
         if c in sfxs:
             sfxs1 = sfxs[c]
             for sfx in sfxs1:
                 s, kod, ending, replace, info = sfx
                 if c in code and c == kod and ending != 'Y':
-                    pass
+                    if '[' in info:
+                        end = info.index(']')
+                        inside = info[1:end]
+                        if inside[0] == '^':
+                            ignore = inside[1:]
+                            accept = info[end+1:]
+                            if w[-1] in ignore + accept:
+                                continue
+                        else:
+                            accept = inside
+                    else:
+                        accept = info
+                    if accept == '':
+                        if ending == '0':
+                            conj.append(w + replace)
+                        elif ending == w[-len(ending):]:
+                             conj.append(w[:-len(ending)] + replace)
+                    elif w[-len(accept):] == accept:
+                        if ending == '0':
+                            conj.append(w + replace)
+                        elif ending == w[-len(ending):]:
+                             conj.append(w[:-len(ending)] + replace)
     return conj
     
 def make_word_list():
@@ -32,16 +55,13 @@ def make_word_list():
     sfxs = [x.replace('  ', ' ').split(' ') for x in aff if x[:3] == 'SFX']
     sfxs = [(x + [' '])[:5] for x in sfxs]
     d = {}
-    for kod in 'ABCDE':
+    for kod in 'CDEFG':
         value = [x for x in sfxs if x[1] == kod]
         d.update({kod: value})
     sfxs = d
     for word in d1:
         s = word.split('/')
         w = s[0]
-        if w == 'henne':
-            print(w)
-        out.append(w)        
         if any([str(i) in w for i in range(10)]):
             continue
         if any([x in w for x in '-:.,/!']):
@@ -49,17 +69,20 @@ def make_word_list():
         if len([x for x in w if x.isupper()]):
             continue        if len(s) >= 2:
             w = s[0]
+            if w[:4] == 'afro':
+                print(w)
             code = s[1]
-            code = code.split(' ')
-            conjugation = conjugate_word(w, code)
-            for x in conjugation:
+            code = code.split(' ')[0]
+            conj = conjugate_word(w, code)
+            out.append(w)                      
+            for x in conj:
                 out.append(x)
     return out
 
 if __name__ == "__main__":
     out = make_word_list()
     out1 = '\n'.join([x for x in out if len(x) == 5])
-    with open('sv_SE.txt', 'w', encoding='utf-8') as fout:
+    with open('sv_SE2.txt', 'w', encoding='utf-8') as fout:
         fout.write(out1) 
     print('done')
     
