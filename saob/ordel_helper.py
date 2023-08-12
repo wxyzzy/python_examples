545ebdb7c266777c532bab7b567b53edfe078f37
James Nash <james.xxyzzy@gmail.com>
2023-08-12 11:50:07 +0200

    First row of selection area works

--- saob/ordel_helper.py
+++ saob/ordel_helper.py
@@ -10,7 +10,15 @@ from functools import partial
 # In frame1 the current row and char index is:
 char_index = 0
 row_index = 0
-lbl = None
+
+#button in the selection area
+lbl = []
+label_color = []
+label_char = []
+undefined_color = '#333'
+exclude_color = '#000'
+required_color = '#088'
+pattern_color = '#3a3'
 pattern = '_____'
 required = ''
 excluded = ''
@@ -32,10 +40,13 @@ def main():
         for j in range(5):
             index = j + i * 5
             foo = partial(color_selection, index)
-            a = (tk.Button(frame1, bg='#333', fg='#ffa', width=4, height=2, padx=0, pady=0, 
-                          font=('calibre', 12, 'bold'), justify=tk.CENTER, command=foo))
+            a = (tk.Button(frame1, bg=undefined_color, fg='#ffa', highlightcolor=undefined_color,
+                           width=4, height=2, activebackground=undefined_color,
+                           font=('calibre', 12, 'bold'), justify=tk.CENTER, command=foo))
             a.grid(row=i, column=j, padx=2, pady=2)
             lbl.append(a)
+            label_color.append(undefined_color)
+            label_char.append('')
     
     # keyboard
     frame2 = tk.Frame(window, height=300, width=450, bg="#555")
@@ -46,33 +57,47 @@ def main():
             keys = 'qwertyuiopåasdfghjklöäzxcvbnm'
             if index < len(keys):
                 ch = keys[index]
-                a = (tk.Label(frame2, bg='#000', fg='#ffa', width=4, height=2, padx=0, pady=0,
-                              font=('calibre', 12, 'bold'), text=ch, justify=tk.CENTER))
+                a = (tk.Label(frame2, bg=undefined_color, fg='#ffa', width=4, height=2, padx=0, pady=0,
+                              font=('calibre', 12, 'bold'), text=ch, justify=tk.CENTER, relief=tk.RAISED))
                 a.grid(row=i, column=j, padx=2, pady=2)
                 #lbl.append(a)
 
-    btn = tk.Button(window, text="Spela", bg='#333', fg='#ffa', command=play)
+    btn = tk.Button(window, text="Spela", bg=undefined_color, fg='#ffa', command=play)
     btn.grid(row=2, column=1)
     
     # hint
     frame3 = tk.Frame(window, height=100, width=450, bg="#555")
     frame3.grid(row=3, column=1, padx=4, pady=4, columnspan=2)
     text = 'hint - words found: 0'
-    n_label = tk.Label(frame3, bg='#000', fg='#ffa', width=100, height=2, padx=2, pady=2, text=text, justify=tk.CENTER)
+    n_label = tk.Label(frame3, bg=undefined_color, fg='#ffa', width=100, height=2, padx=2, pady=2, text=text, justify=tk.CENTER)
     n_label.grid(row=1, column=0, padx=2, pady=2, sticky=tk.W)
     text = '[]'
-    word_label = tk.Label(frame3, bg='#000', fg='#ffa', width=100, height=2, padx=2, pady=2, text=text, justify=tk.CENTER)
+    word_label = tk.Label(frame3, bg=undefined_color, fg='#ffa', width=100, height=2, padx=2, pady=2, text=text, justify=tk.CENTER)
     word_label.grid(row=2, column=0, padx=2, pady=2, sticky=tk.W)
     window.bind_all('<Key>', key)
     window.mainloop()
 
 def color_selection(index):
-    global lbl
+    global lbl, label_color
     b = lbl[index]
-    b.config(bg = '#088')
+    if label_color[index] == undefined_color: label_color[index] = exclude_color
+    elif label_color[index] == exclude_color: label_color[index] = required_color
+    elif label_color[index] == required_color: label_color[index] = pattern_color
+    elif label_color[index] == pattern_color: label_color[index] = undefined_color
+    b.config(bg = label_color[index])
     return
 
 def play():
+    # collect information
+    # for now, assume first row only
+    pattern = ''
+    required = ''
+    excluded = ''
+    for i in range(5):
+        pattern += label_char[i] if label_color[i] == pattern_color else '_'
+        required += label_char[i] if label_color[i] == required_color else ''
+        excluded += label_char[i] if label_color[i] == exclude_color else ''
+    
     # compose query
     # 'ordel -i 5 -p __ta_ -r u -e ee -z
     r = f'-r {required}' if required else ''
@@ -92,6 +117,7 @@ def use_char(ch):
         ch in 'qwertyuiopåasdfghjklöäzxcvbnm'):
         index = char_index + row_index * 5
         lbl[index].config(text=ch)
+        label_char[index] = ch
         char_index += 1
     elif ch in 'BS' and char_index > 0:
         char_index -= 1
